399dd49841314a39e00200897dbf75b16e53c8da
intandeay <intandea@gmail.com>
2018-02-22 10:14:52 +0700

    first upload

---
+++ server.R
@@ -0,0 +1,74 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(lattice)
+
+
+studInfo <- read.csv("studentInfo.csv")
+
+colorList <- c("darkblue", "red", "darkgreen", "gold")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  dataset <- reactive({
+    return(studInfo[sample(nrow(studInfo), input$sampleSize), ])
+  })
+  
+  output$plot1 <- renderPlot({
+    
+    new_df <- data.frame("isPassed" = ifelse(dataset()$final_result == "Distinction" |
+                                            dataset()$final_result == "Pass", 1, 0),
+                      "par1" = as.factor(dataset()[,c(input$par1)]),
+                      "groupby" = as.factor(dataset()[,c(input$groupby)])
+                      )
+    
+    sum_table <- group_by(new_df, groupby, par1) %>% summarize(isPassed = sum(isPassed))
+    
+    p <- ggplot(sum_table, aes(par1, isPassed, fill = par1)) + geom_bar(stat = "identity")
+    input_x1 <- toupper(input$par1)
+    p <- p + ggtitle(paste("Total Passing Students by ", input$par1, " and ", input$groupby)) + facet_grid( . ~ groupby) + scale_x_discrete(labels = abbreviate) + theme(axis.text.x = element_text(angle = 90, hjust = 1))  + labs(y = "Total Students Passed", x = "") + guides(fill=guide_legend(title=paste("", input_x1)))
+    print(p)
+  })
+   
+  output$summaryTitle <- renderText({
+    paste("Summary of ", input$sampleSize, " samples")
+  })
+  
+  output$summary <- renderPrint({
+    summary(dataset())
+  })
+  output$plot2 <-renderPlot({
+    
+    infoModule <- studInfo[studInfo$code_module == input$module,]
+    count_res <- table(infoModule$final_result, infoModule[,c(input$par2)])
+                                          
+    par(mar=c(10.1, 4.1, 2.1, 8.1), xpd=TRUE)
+    barplot(count_res, 
+            main=paste("Final Result Distribution by ", input$par2, " from course ", input$module), 
+            ylab = "Number of Students", las = 2,
+            col=colorList
+    )
+    legend("topright", inset=c(-0.1,0),
+           legend = rownames(count_res),
+           fill = colorList
+    )
+  })
+  
+  selectedCourse <- eventReactive(input$summary, {
+    sample[sample$code_module == input$module,]
+  })
+  
+  
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,106 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+studInfo <- read.csv("studentInfo.csv")
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Open University Learning Analytics Dataset Exploration"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      
+      h3("Parameter Selection"),
+      h4("General Passing Rate"),
+      
+      sliderInput('sampleSize', 'Choose Sample Size', min=500, max=nrow(studInfo),
+                  value=min(500, nrow(studInfo)), step=100, round=0),
+      
+      selectInput("groupby", "Choose a Parameter to Group By:",
+                  list(`Gender` = "gender",
+                       `Region` = "region",
+                       `Highest Education` = "highest_education",
+                       `IMD Band` = "imd_band",
+                       `Age Band` = "age_band",
+                       `No. of Previous Attempts` = "num_of_prev_attempts",
+                       `Disability` = "disability"
+                  ),
+                  selected = "highest_education"
+      ),
+      
+      selectInput("par1", "Choose Another Parameter:",
+                  list(`Gender` = "gender",
+                       `Region` = "region",
+                       `Highest Education` = "highest_education",
+                       `IMD Band` = "imd_band",
+                       `Age Band` = "age_band",
+                       `No. of Previous Attempts` = "num_of_prev_attempts",
+                       `Disability` = "disability"
+                  )
+      ),
+      
+      h4("Course-Based Passing Result"),
+      selectInput("module", "Choose a module:",
+                  list(`AAA` = "AAA",
+                       `BBB` = "BBB",
+                       `CCC` = "CCC",
+                       `DDD` = "DDD",
+                       `EEE` = "EEE",
+                       `FFF` = "FFF",
+                       `GGG` = "GGG"
+                  )
+      ),
+      selectInput("par2", "Choose a Parameter:",
+                  list(`Gender` = "gender",
+                       `Region` = "region",
+                       `Highest Education` = "highest_education",
+                       `IMD Band` = "imd_band",
+                       `Age Band` = "age_band",
+                       `No. of Previous Attempts` = "num_of_prev_attempts",
+                       `Disability` = "disability"
+                  ),
+                  selected = "region"
+      )
+      #actionButton("summary", "Show Summary")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      
+      tabsetPanel(type = "tabs",
+                  tabPanel("General Passing Rate", br(), 
+                           p("This application is based on Open University Learning Dataset. The dataset is taken from:"),
+                           a("https://archive.ics.uci.edu/ml/datasets/Open+University+Learning+Analytics+dataset#"),
+                           p("It contains data about courses, students and their interactions with Virtual Learning Environment (VLE) for seven selected courses (called modules)."),
+                           p("This application reads the studentInfo.csv, which stores the information of final result of the assessment from the seven courses."),
+                           p("This tab is basically showing the relationship between number of students passing the assessment from the course and two other variables, taken from some random samples which user specifies the amount on the left."),
+                           p("Student is considered passed if his/her final result is either passed or distinction."),
+                           plotOutput("plot1"),
+                           br(),
+                           textOutput("summaryTitle"),
+                           verbatimTextOutput("summary"),
+                           tags$head(tags$style("#summary{font-size:10px}"))
+                           ),
+                  tabPanel("Course-Based Passing Result", br(), 
+                           p("This application is based on Open University Learning Dataset. The dataset is taken from:"),
+                           a("https://archive.ics.uci.edu/ml/datasets/Open+University+Learning+Analytics+dataset#"),
+                           p("It contains data about courses, students and their interactions with Virtual Learning Environment (VLE) for seven selected courses (called modules)."),
+                           p("This application reads the studentInfo.csv, which stores the information of final result of the assessment from the seven courses."),
+                           p("This tab is showing the relationship between final result of the assessment and a variable picked by user from a specific course."),
+                           plotOutput("plot2"))
+      )
+   
+    )
+  )
+  )
+)
